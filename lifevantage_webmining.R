ab051a268a40ef2f53271d78b57f4910fb43eb09
Jason Miller <jmillerlaureate@gmail.com>
2014-09-03 16:56:15 -0400

    .

--- lifevantage_webmining.R
+++ lifevantage_webmining.R
@@ -7,12 +7,37 @@
 # Load Functions and Libraries --------------------------------------------
 source("lifevantage_functions.R")
 
-# Scrape Google Scholar ---------------------------------------------------
-gs_protandim <- get_google_scholar_df( u = "http://scholar.google.com/scholar?as_q=protandim")
+# Scrape Google Finance ---------------------------------------------------
+googlefinance <- WebCorpus(GoogleFinanceSource("NASDAQ:LFVN"))
+
+# Scrape Google News ------------------------------------------------------
+lv.googlenews <- WebCorpus(GoogleNewsSource("LifeVantage"))
+p.googlenews <- WebCorpus(GoogleNewsSource("Protandim"))
+ts.googlenews <- WebCorpus(GoogleNewsSource("TrueScience"))
+
+# Scrape NYTimes ----------------------------------------------------------
+lv.nytimes <- WebCorpus(NYTimesSource("LifeVantage", appid = nytimes_appid))
+p.nytimes <- WebCorpus(NYTimesSource("Protandim", appid = nytimes_appid))
+ts.nytimes <- WebCorpus(NYTimesSource("TrueScience", appid = nytimes_appid))
+
+# Scrape Reuters ----------------------------------------------------------
+lv.reutersnews <- WebCorpus(ReutersNewsSource("LifeVantage"))
+p.reutersnews <- WebCorpus(ReutersNewsSource("Protandim"))
+ts.reutersnews <- WebCorpus(ReutersNewsSource("TrueScience"))
+
+# Scrape Yahoo! Finance ---------------------------------------------------
+lv.yahoofinance <- WebCorpus(YahooFinanceSource("LFVN"))
 
 # Scrape Yahoo! News ------------------------------------------------------
+lv.yahoonews <- WebCorpus(YahooNewsSource("LifeVantage"))
+p.yahoonews <- WebCorpus(YahooNewsSource("Protandim"))
+ts.yahoonews <- WebCorpus(YahooNewsSource("TrueScience"))
 
+# Scrape Yahoo! Inplay ----------------------------------------------------
+lv.yahooinplay <- WebCorpus(YahooInplaySource("LifeVantage"))
 
-# Scrape Google Finance ---------------------------------------------------
+# Scrape Google Scholar ---------------------------------------------------
+gs_protandim   <- get_google_scholar_df( u = "http://scholar.google.com/scholar?as_q=protandim")
+gs_lifevantage <- get_google_scholar_df( u = "http://scholar.google.com/scholar?as_q=lifevantage")
 
 
